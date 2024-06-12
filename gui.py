aba97cfecd9fe56e49d111204877dcb913b2f1a8
Erdianti Wiga Putri Andini <13522053@mahasiswa.itb.ac.id>
2024-06-12 09:25:40 +0700

    fix: gui

--- gui.py
+++ gui.py
@@ -1,29 +1,48 @@
-from tkinter import Tk, Button, Label
-from PIL import ImageTk, Image
+from tkinter import Tk, Button, Label, Entry
 import geopandas as gpd
 import osmnx as ox
 import webbrowser
-from tkinter import Tk, Button
 
 def plot_route(G, path):
     nodes, edges = ox.graph_to_gdfs(G, nodes=True, edges=True)
     route_nodes = nodes.loc[path]
-
     m = route_nodes.explore()
     m.save('map.html')
 
-
-def show_gui():
+def show_gui(process_route_callback):
     root = Tk()
     root.title("Route Network")
+    root.geometry('500x320') 
+    bg_color = '#f9dced'
+
+    root.configure(bg=bg_color)
+
+    Label(root, text="City:", bg=bg_color, fg='black').pack(pady=(10, 0))
+    city_entry = Entry(root)
+    city_entry.pack(pady=(0, 10))
+
+    Label(root, text="Start Address:", bg=bg_color, fg='black').pack(pady=(10, 0))
+    start_entry = Entry(root)
+    start_entry.pack(pady=(0, 10))
+
+    Label(root, text="End Address:", bg=bg_color, fg='black').pack(pady=(10, 0))
+    end_entry = Entry(root)
+    end_entry.pack(pady=(0, 10))
+
+    def on_submit():
+        city = city_entry.get()
+        start_addr = start_entry.get()
+        end_addr = end_entry.get()
+        process_route_callback(city, start_addr, end_addr)
+        open_map()  
+
+    submit_button = Button(root, text="Find Route", bg='#d6aec6', command=on_submit)
+    submit_button.pack(pady=(10, 5))
 
     def open_map():
         webbrowser.open("map.html")
 
-    open_button = Button(root, text="Show Map", command=open_map)
-    open_button.pack()
-
-    close_button = Button(root, text="Close", command=root.quit)
-    close_button.pack()
+    close_button = Button(root, text="Close", bg='#d6aec6', command=root.quit)
+    close_button.pack(pady=(5, 10))
 
     root.mainloop()
--- main.py
+++ main.py
@@ -3,19 +3,15 @@ from astar import astar
 from gui import plot_route, show_gui
 import osmnx as ox
 
-def main():
-    city = 'Bandung, Indonesia'
+def process_route(city, start_addr, end_addr):
     G = download_road(city)
-    
-    start_addr = "Jalan Asia Afrika, Bandung"
-    end_addr = "Bandung Institute of Technology, Bandung"
-
     start_node = ox.distance.nearest_nodes(G, ox.geocoder.geocode(start_addr)[1], ox.geocoder.geocode(start_addr)[0])
     end_node = ox.distance.nearest_nodes(G, ox.geocoder.geocode(end_addr)[1], ox.geocoder.geocode(end_addr)[0])
-    
     path = astar(G, start_node, end_node)
     plot_route(G, path)
-    show_gui()
+
+def main():
+    show_gui(process_route)
 
 if __name__ == "__main__":
     main()
