ef57f9a1501c9b680569b4567bf5a54796cea813
Erdianti Wiga Putri Andini <13522053@mahasiswa.itb.ac.id>
2024-06-12 12:43:42 +0700

    feat: final last tc

--- gui.py
+++ gui.py
@@ -1,7 +1,8 @@
-from tkinter import Tk, Button, Label, Entry
+from tkinter import Tk, Button, Label, Entry, StringVar
 import geopandas as gpd
 import osmnx as ox
 import webbrowser
+import time
 
 def plot_route(G, path):
     nodes, edges = ox.graph_to_gdfs(G, nodes=True, edges=True)
@@ -12,32 +13,39 @@ def plot_route(G, path):
 def show_gui(process_route_callback):
     root = Tk()
     root.title("Route Network")
-    root.geometry('500x320') 
+    root.geometry('500x310') 
     bg_color = '#f9dced'
 
     root.configure(bg=bg_color)
 
+    time_var = StringVar()
+    # time_var.set("Execution Time: 0 seconds")
+
     Label(root, text="City:", bg=bg_color, fg='black').pack(pady=(10, 0))
-    city_entry = Entry(root)
+    city_entry = Entry(root, width=50)
     city_entry.pack(pady=(0, 10))
 
     Label(root, text="Start Address:", bg=bg_color, fg='black').pack(pady=(10, 0))
-    start_entry = Entry(root)
+    start_entry = Entry(root, width=50)
     start_entry.pack(pady=(0, 10))
 
     Label(root, text="End Address:", bg=bg_color, fg='black').pack(pady=(10, 0))
-    end_entry = Entry(root)
+    end_entry = Entry(root, width=50)
     end_entry.pack(pady=(0, 10))
 
     def on_submit():
         city = city_entry.get()
         start_addr = start_entry.get()
         end_addr = end_entry.get()
-        process_route_callback(city, start_addr, end_addr)
+        exec_time = process_route_callback(city, start_addr, end_addr)
+        time_var.set(f"Execution Time: {exec_time:.2f} seconds")
         open_map()  
 
     submit_button = Button(root, text="Find Route", bg='#d6aec6', command=on_submit)
     submit_button.pack(pady=(10, 5))
+
+    exec_time_label = Label(root, textvariable=time_var, bg=bg_color, fg='black')
+    exec_time_label.pack()
 
     def open_map():
         webbrowser.open("map.html")
--- main.py
+++ main.py
@@ -2,13 +2,18 @@ from data import download_road
 from astar import astar
 from gui import plot_route, show_gui
 import osmnx as ox
+import time
 
 def process_route(city, start_addr, end_addr):
+    start_time = time.time()
     G = download_road(city)
     start_node = ox.distance.nearest_nodes(G, ox.geocoder.geocode(start_addr)[1], ox.geocoder.geocode(start_addr)[0])
     end_node = ox.distance.nearest_nodes(G, ox.geocoder.geocode(end_addr)[1], ox.geocoder.geocode(end_addr)[0])
     path = astar(G, start_node, end_node)
     plot_route(G, path)
+    end_time = time.time()
+    execution_time = end_time - start_time
+    return execution_time
 
 def main():
     show_gui(process_route)
